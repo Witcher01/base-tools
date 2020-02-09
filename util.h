a338611876e479e36c5adbc003912658e6d2f744
Witcher01 <witcher01@posteo.net>
2020-02-09 19:24:41 +0100

    added include guard

--- util.h
+++ util.h
@@ -1,3 +1,6 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 /* define some standard macros */
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
@@ -5,3 +8,5 @@
 void *emalloc(size_t);
 void *ecalloc(size_t, size_t);
 void fail(const char *, ...);
+
+#endif /* UTIL_H */
